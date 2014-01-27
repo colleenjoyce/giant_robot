74b76808df7bf0ebe5b2734854a6009d3eae269f
colleenjoyce <colleenelizabethjoyce@gmail.com>
2014-01-27 14:05:11 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GiantRobot::Application.config.secret_token = '928aef1126d69f30a33bf0683b29cdba88949a74ecc751a1b738e3e854b17b8900171432c83855733f2c79202605a3d581266a94155993edf0292bc592d53721'
