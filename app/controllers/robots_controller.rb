842f07670ec69f25c02e10886018dcb2958974b5
colleenjoyce <colleenelizabethjoyce@gmail.com>
2014-01-28 13:36:05 -0500

    updated files

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -1,23 +1,16 @@
-<<<<<<< HEAD
 class RobotsController < ApplicationController
 
-def index
-	@robots = Robot.all 
-end
+	def index
+		@robots = Robot.all 
+	end
 
-def create
-	name = params[:name] 
-	Robot.create(name: name)
-end
+	def create
+		name = params[:name] 
+		Robot.create(name: name)
+	end
 
-end
-=======
-class RobotsController < ActionController::Base
- 	
 	def destroy
 		@robot
 	end
 
 end
- 
->>>>>>> 38e3e54cbe46e94d2f9b7b952f88cb44d073bff6
